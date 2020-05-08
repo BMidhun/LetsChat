cdb38bd2891ee8b3f5eeb62df2c4cafb59a7a4fb
BMidhun <midhunbabygeorge@gmail.com>
2020-05-08 16:59:04 +0530

    Time issue fixed

--- utils/messages.js
+++ utils/messages.js
@@ -2,7 +2,7 @@ const moment = require('moment')
 
 function formatMessage (username, text) {
 
-    let time = moment();
+    let time = moment().utc(true);
     time = moment(time).format("hh:mm");
     return {username,text,time}
 }
